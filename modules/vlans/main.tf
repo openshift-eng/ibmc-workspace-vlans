a3932ce989bf9a415637d1e90ea7b02bae93df29
jimb0 <jimccann@redhat.com>
2023-05-16 11:14:12 -0400

    add new vlan

--- modules/vlans/main.tf
+++ modules/vlans/main.tf
@@ -15,12 +15,12 @@ resource "ibm_network_vlan" "segment_100" {
   tags = var.tags
 }
 
-#resource "ibm_network_vlan" "segment_101" {
-#  name       = "${var.project}-dev-segment-101"
-#  datacenter = var.datacenter
-#  type       = "PRIVATE"
-#  router_hostname = "bcr04a.${var.datacenter}"
-##  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
-#  tags = var.tags
-#}
+resource "ibm_network_vlan" "segment_101" {
+  name       = "${var.project}-dev-segment-101"
+  datacenter = var.datacenter
+  type       = "PRIVATE"
+  router_hostname = "bcr04a.${var.datacenter}"
+#  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
+  tags = var.tags
+}
 
