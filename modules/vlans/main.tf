85f6cc30f715cd6ef48fb3cf6c9e4e75993c829b
jimb0 <jimccann@redhat.com>
2023-05-23 10:49:28 -0400

    adding vlan ssociation with gateway fixed id

--- modules/vlans/main.tf
+++ modules/vlans/main.tf
@@ -28,7 +28,6 @@ resource "ibm_network_vlan" "segment_101" {
   datacenter = var.datacenter
   type       = "PRIVATE"
   router_hostname = "bcr04a.${var.datacenter}"
-#  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
   tags = var.tags
 }
 
