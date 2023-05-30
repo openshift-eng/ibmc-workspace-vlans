4e70b094b3a5aba678ff3d486238c92d7894106d
jimb0 <jimccann@redhat.com>
2023-05-30 15:53:17 -0400

    clean up

--- main.tf
+++ main.tf
@@ -10,25 +10,6 @@ locals {
   ]
 }
 
-#resource "tls_private_key" "ssh" {
-#  algorithm = "RSA"
-#  rsa_bits  = 4096
-#}
-
-#resource "null_resource" "create_private_key" {
-#  provisioner "local-exec" {
-#    command = <<-EOT
-#      echo '${tls_private_key.ssh.private_key_pem}' > ./'${var.project}'.pem
-#      chmod 400 ./'${var.project}'.pem
-#    EOT
-#  }
-#}
-
-#resource "ibm_compute_ssh_key" "project" {
-#  label      = "${var.project}-sshkey"
-#  public_key = tls_private_key.ssh.public_key_openssh
-#  tags       = local.tags
-#}
 
 module "vlans" {
   source     = "./modules/vlans"
@@ -40,21 +21,3 @@ module "vlans" {
 }
 
 
-#module "virtual-machines" {
-##  depends_on   = [module.vlans]
-#  source       = "./modules/compute-virtual"
-#  count        = var.vsicount 
-#  name         = "${var.project}-virtual-instance-${count.index}"
-#  datacenter   = var.datacenter
-#  domain_name  = var.domain_name
-##  public_vlan  = 3332332
-##  public_vlan  = module.vlans.public_compute_vlan.id
-##  private_vlan = 3332338
-#  private_vlan = var.vsi_private_vlan
-##  private_vlan = module.vlans.private_compute_vlan.id
-#  local_disk   = false
-#  ssh_key_ids  = local.ssh_key_ids
-#  tags         = local.tags
-#  psgids        = var.psgids
-#}
-#
--- modules/vlans/outputs.tf
+++ modules/vlans/outputs.tf
@@ -13,7 +13,7 @@ output "segment_100" {
   value = ibm_network_vlan.segment_100
 }
 
-#output "segment_101" {
-#  description = "Returns the private VLAN info."
-#  value = ibm_network_vlan.segment_101
-#}
+output "segment_101" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_101
+}
