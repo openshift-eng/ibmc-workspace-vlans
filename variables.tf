2c9bd921e04436e347d52f7b45fee45466ceb2e8
jimb0 <jimccann@redhat.com>
2023-05-30 15:50:05 -0400

    clean up

--- variables.tf
+++ variables.tf
@@ -20,40 +20,6 @@ variable "project" {
   type        = string
 }
 
-#variable "existing_ssh_key" {
-#  description = "The name of an existing SSH key to use for the virtual machines. If none is provided, one will be generated."
-#  type        = string
-#  default     = ""
-#}
-
-#variable "domain_name" {
-#  description = "The domain name to use for all resources."
-#  type        = string
-#  default     = "example.com"
-#}
-
-#variable "vsicount" {
-#  description = "Number of VSI systems"
-#  type        = string
-#  default     = "1"
-#}
-
-#variable "psgids" {
-#  description = "public security group ids VSI systems"
-#  type        = list
-#  default     = []
-#}
-
-#variable "vm-post-install-script-uri" {
-# default = "https://binaries.twingate.com/connector/setup.sh"
-#}
-
-
-#variable "vsi_private_vlan" {
-#  description = "Private VLAN of VSI systems"
-#  type        = string
-#  default     = ""
-#}
 
 variable "gateway_id" {
   description = "ID of gateway systems"
