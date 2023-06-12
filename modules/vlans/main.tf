5493e8697ad6f9c5cfac710886747e55474601bb
jimb0 <jimccann@redhat.com>
2023-06-12 10:21:44 -0400

    update ip segments ver2

--- modules/vlans/main.tf
+++ modules/vlans/main.tf
@@ -21,29 +21,42 @@ resource "ibm_network_gateway_vlan_association" "gateway_vlan_association_dal_pr
 
 locals {
  vlansegments = {
-   "221" = { resource = "segment_221", name = "sgement-221", association = "gateway_vlan_association_221" },
-   "222" = { resource = "segment_222", name = "sgement-222", association = "gateway_vlan_association_222" }
+   "223" = { resource = "segment_223", name = "sgement-223", association = "gateway_vlan_association_223" }
  }
 }
 
 
 
-resource "ibm_network_vlan" "segment_100" {
-  name       = "${var.project}-segment-100"
-  datacenter = var.datacenter
-  type       = "PRIVATE"
-  router_hostname = "bcr01a.${var.datacenter}"
-#  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
-  tags = var.tags
+#### needed to fix
+#resource "ibm_network_vlan" "segment_100" {
+#  name       = "${var.project}-segment-100"
+#  datacenter = var.datacenter
+#  type       = "PRIVATE"
+#  router_hostname = "bcr01a.${var.datacenter}"
+##  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
+#  tags = var.tags
+#}
+#
+#resource "ibm_network_gateway_vlan_association" "gateway_vlan_association_100" {
+#  gateway_id      = var.gateway_id 
+#  network_vlan_id = ibm_network_vlan.segment_100.id
+#  bypass          = false
+#}
+
+moved {
+  from = ibm_network_vlan.segment_100
+  to = ibm_network_vlan.segment_221
 }
 
-resource "ibm_network_gateway_vlan_association" "gateway_vlan_association_100" {
-  gateway_id      = var.gateway_id 
-  network_vlan_id = ibm_network_vlan.segment_100.id
-  bypass          = false
+moved {
+  from = ibm_network_gateway_vlan_association.gateway_vlan_association_100
+  to = ibm_network_gateway_vlan_association.gateway_vlan_association_221
 }
+#### needed to fix
 
 
+
+#### needed to fix
 #resource "ibm_network_vlan" "segment_101" {
 #  name       = "${var.project}-segment-101"
 #  datacenter = var.datacenter
@@ -68,6 +81,24 @@ resource "ibm_network_gateway_vlan_association" "gateway_vlan_association_100" {
 #  from = ibm_network_gateway_vlan_association.gateway_vlan_association_101
 #  to = ibm_network_gateway_vlan_association.gateway_vlan_association_222
 #}
+#### needed to fix
+
+
+resource "ibm_network_vlan" "segment_221" {
+  name       = "${var.project}-segment-221"
+  datacenter = var.datacenter
+  type       = "PRIVATE"
+  router_hostname = "bcr01a.${var.datacenter}"
+#  router_hostname = replace(ibm_network_vlan.public.router_hostname, "/^f/", "b")
+  tags = var.tags
+}
+
+resource "ibm_network_gateway_vlan_association" "gateway_vlan_association_221" {
+  gateway_id      = var.gateway_id
+  network_vlan_id = ibm_network_vlan.segment_221.id
+  bypass          = false
+}
+
 
 
 resource "ibm_network_vlan" "segment_222" {
@@ -86,3 +117,5 @@ resource "ibm_network_gateway_vlan_association" "gateway_vlan_association_222" {
   network_vlan_id = ibm_network_vlan.segment_222.id
   bypass          = false
 }
+
+
