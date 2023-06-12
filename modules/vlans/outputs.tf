c2335602f1404741414992887e567bc509d3232c
jimb0 <jimccann@redhat.com>
2023-06-12 12:36:01 -0400

    update ip segments

--- modules/vlans/outputs.tf
+++ modules/vlans/outputs.tf
@@ -17,3 +17,70 @@ output "segment_222" {
   description = "Returns the private VLAN info."
   value = ibm_network_vlan.segment_222
 }
+
+
+output "segment_223" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_223
+}
+
+
+output "segment_224" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_224
+}
+
+
+output "segment_225" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_225
+}
+
+
+output "segment_226" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_226
+}
+
+
+output "segment_227" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_227
+}
+
+
+output "segment_228" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_228
+}
+
+
+output "segment_229" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_229
+}
+
+
+output "segment_230" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_230
+}
+
+
+output "segment_231" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_231
+}
+
+
+output "segment_232" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_232
+}
+
+
+output "segment_234" {
+  description = "Returns the private VLAN info."
+  value = ibm_network_vlan.segment_234
+}
+
